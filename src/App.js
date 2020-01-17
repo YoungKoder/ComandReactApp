9409b50c319fbd714eabd456365067e0b2379300
Maksym Teslenko <maksteslenko924@gmail.com>
2020-01-18 01:51:03 +0200

    ading new page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 
-import "./App.css";
-
 import HelloPage from "./components/pages/helloPage/helloPage"
 import SigninPage from "./components/pages/loginPages/signinPage"
 import SignupPage from "./components/pages/loginPages/signupPage"
+import ContentPage from "./components/pages/contentPage/contentPage"
 import UserProfile from "./components/pages/user/UserPage";
 import Newslist from "./components/pages/news/Newslist";
 import Eventslist from "./components/pages/events/EventsList";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
 const App = () => {
   return (
@@ -17,6 +16,7 @@ const App = () => {
           <Route path="/" exact component={HelloPage} />
           <Route path="/sign-in" component={SigninPage} />
           <Route path="/sign-up" component={SignupPage} />
+          <Route path="/content-page" component={ContentPage} />
           <Route path="/newslist" component={Newslist} />
           <Route path="/eventslist" component={Eventslist} />
           <Route path="/userprofile" component={UserProfile} />
---
+++ src/components/pages/contentPage/contentPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ContentPage =()=>{
+    return (
+        <h1></h1>
+    )
+}
+
+export default ContentPage;
